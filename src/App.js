9873efa281617b9230ec941e71178d2a7e51220f
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-23 18:46:41 +0200

    Getting geolocation of user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [lat, setLat] = useState('')
+  const [long, setLong] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude)
+      setLong(position.coords.longitude)
+    })
+  })
+
   const location = 'Berlin, Germany'
 
   return (
     <>
       <h1>Hello world!</h1>
       <p>Your location is {location}</p>
+      <p>Your latitude is {lat}</p>
+      <p>Your longitude is {long}</p>
     </>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,20 +3,15 @@ import App from './App';
 
 test('renders title', () => {
   render(<App />);
-  const h1 = screen.getByText(/Hello world!/i);
+  const h1 = screen.getByText(/Hello world!/);
   expect(h1).toBeInTheDocument();
 });
 
 test('gets location', () => {
-  render(<App />)
-  const location = 'Berlin, Germany'
-  expect(location).toContain('Berlin, Germany')
 })
 
 test('I can see current weather', () => {
-
 })
 
 test('I can change from celsius to fahrenheit', () => {
-
 })
