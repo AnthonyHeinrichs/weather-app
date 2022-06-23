6dc6d31c7e6c04430424531727c65dc4b6bf9b8a
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-23 19:49:05 +0200

    Retrieving data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
+
+const WeatherAPI_endpoint = 'https://api.openweathermap.org/data/2.5/weather?'
+const API_key = '82401161e4362bdbac9573f0b61d3f82'
 
 function App() {
   const [lat, setLat] = useState('')
   const [long, setLong] = useState('')
+  const [responseData, setResponseData] = useState('')
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLat(position.coords.latitude)
       setLong(position.coords.longitude)
     })
-  })
 
-  const location = 'Berlin, Germany'
+    axios.get(`${WeatherAPI_endpoint}lat=${lat}&lon=${long}&appid=${API_key}`)
+    .then((response) => {
+      setResponseData(response.data)
+    })
+
+    console.log(responseData)
+
+  }, [lat, long])
 
   return (
     <>
       <h1>Hello world!</h1>
-      <p>Your location is {location}</p>
-      <p>Your latitude is {lat}</p>
-      <p>Your longitude is {long}</p>
+      <h2>{responseData.name}</h2>
+      <p>{responseData.weather[0].main}</p>
     </>
   )
 }
