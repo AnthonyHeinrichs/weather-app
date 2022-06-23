c07d3cda95723c9c5505064bb67b9ba7c5528857
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-23 18:14:22 +0200

    Cleanup and initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const location = 'Berlin, Germany'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Hello world!</h1>
+      <p>Your location is {location}</p>
+    </>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1 = screen.getByText(/Hello world!/i);
+  expect(h1).toBeInTheDocument();
 });
+
+test('gets location', () => {
+  render(<App />)
+  const location = 'Berlin, Germany'
+  expect(location).toContain('Berlin, Germany')
+})
+
+test('I can see current weather', () => {
+
+})
+
+test('I can change from celsius to fahrenheit', () => {
+
+})
