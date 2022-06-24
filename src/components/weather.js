82fa307e460f86f5eec6c8faece821f6aedc36d5
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-24 23:06:47 +0200

    No internet, need google to style more

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,8 +5,8 @@ const Weather = ({weatherData}) => {
   return (
     <div className='main'>
       <h2 className='location'>{weatherData.data.location}</h2>
-      <h2>{weatherData.data.temp}</h2>
-      <h2>{weatherData.data.weather}</h2>
+      <h2 className='temp'>{weatherData.data.temp} C</h2>
+      <h2 className='weather'>{weatherData.data.weather}</h2>
     </div>
   )
 }
