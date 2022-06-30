147c6f5f3481b123fa876f9d13206848d5fa2f49
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-29 20:04:12 -0400

    Fix icon location

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,7 +4,7 @@ const Weather = ({weatherData}) => {
   console.log(weatherData)
   return (
     <div className='main'>
-      <p className='location'>{weatherData.name}</p>
+      <h1 className='location'>{weatherData.name}</h1>
       <p className='weather'>{weatherData.weather[0].main}</p>
       <p className='temp'>{parseInt(weatherData.main.temp)} °C</p>
       <img className='icon' src={weatherData.weather[0].icon} alt='weather-icon' />
