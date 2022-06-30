a3ecebecaa3738adbb09f262966f0261f3272e21
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-29 20:23:53 -0400

    Check if temp is celsius or fahrenheit

--- src/components/weather.js
+++ src/components/weather.js
@@ -10,7 +10,7 @@ const Weather = ({weatherData}) => {
       <p className='weather'>{weatherData.weather[0].main}</p>
       <div className='temp'>
         <p>{parseInt(weatherData.main.temp)}</p>
-        <button className='btn'>°C</button>
+        <button className='btn' onClick={handleClick}>{ temp === 'celcius' ? '°C' : '°F' }</button>
       </div>
       <img className='icon' src={weatherData.weather[0].icon} alt='weather-icon' />
     </div>
